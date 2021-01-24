8a62a4c79bea5f1353b02b091e2f7d00752538e6
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-01-24 17:48:05 +0900

    [add] 表示・選択する領域を確保

--- project/src/pages/index.vue
+++ project/src/pages/index.vue
@@ -1,165 +1,113 @@
 <template>
   <v-row justify="center" align="center">
 
-  <!-- ランキング表示 -->
-  <v-col cols="12" sm="8" md="6">
-    <v-card>
-      <!-- カードタイトル -->
-      <v-card-title class="headline">
-        Welcome to the Vuetify + Nuxt.js template
-      </v-card-title>
-      <!-- カードテキスト -->
-      <v-card-text>
-        <img width="75%" height="75%" src="@/assets/index.png">
-        <p>
-          Vuetify is a progressive Material Design component framework for
-          Vue.js. It was designed to empower developers to create amazing
-          applications.
-        </p>
-        <p>
-          For more information on Vuetify, check out the
-          <a
-            href="https://vuetifyjs.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            documentation </a
-          >.
-        </p>
-        <p>
-          If you have questions, please join the official
-          <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="chat"
-          >
-            discord </a
-          >.
-        </p>
-        <p>
-          Find a bug? Report it on the github
-          <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="contribute"
-          >
-            issue board </a
-          >.
-        </p>
-        <p>
-          Thank you for developing with Vuetify and I look forward to bringing
-          more exciting features in the future.
-        </p>
-        <div class="text-xs-right">
-          <em><small>&mdash; John Leider</small></em>
-        </div>
-        <hr class="my-3" />
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nuxt Documentation
-        </a>
-        <br />
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nuxt GitHub
-        </a>
-      </v-card-text>
-      <!-- v-btnやv-menuなどのカードのactionsを配置するためのコンテナ -->
-      <v-card-actions>
-        <v-spacer />
-        <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-col>
+    <!-- ランキング表示 -->
+    <v-col cols="12" sm="8" md="4" align="start">
+      <v-card>
+        <!-- カードテキスト -->
+        <v-card-text>
+          <p>ここでチャンネル選択</p>
+        </v-card-text>
 
-  <!-- グラフ表示 -->
-  <v-col cols="12" sm="8" md="6">
-    <!-- カード領域 -->
-    <v-card>
-      <!-- カードタイトル -->
-      <v-card-title class="headline">
-        Welcome to the Vuetify + Nuxt.js template
-      </v-card-title>
-      <!-- カードテキスト -->
-      <v-card-text>
-        <img width="75%" height="75%" src="@/assets/index.png">
-        <p>
-          Vuetify is a progressive Material Design component framework for
-          Vue.js. It was designed to empower developers to create amazing
-          applications.
-        </p>
-        <p>
-          For more information on Vuetify, check out the
+        <v-divider></v-divider>
+
+        <v-card-text>
+          <p>ここで期間選択</p>
+        </v-card-text>
+
+        <v-card-text>
+          <p>ここでランキング表示</p>
+        </v-card-text>
+
+        <!-- v-btnやv-menuなどのカードのactionsを配置するためのコンテナ -->
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/messages"> Search Messages </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+    <!-- グラフ表示 -->
+    <v-col cols="12" sm="8" md="8" align="start">
+      <!-- カード領域 -->
+      <v-card>
+        <!-- カードタイトル -->
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+
+        <!-- カードテキスト -->
+        <v-card-text>
+          <img width="75%" height="75%" src="@/assets/index.png">
+          <p>
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
+          </p>
+          <p>
+            For more information on Vuetify, check out the
+            <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation </a
+            >.
+          </p>
+          <p>
+            If you have questions, please join the official
+            <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord </a
+            >.
+          </p>
+          <p>
+            Find a bug? Report it on the github
+            <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board </a
+            >.
+          </p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
           <a
-            href="https://vuetifyjs.com"
+            href="https://nuxtjs.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            documentation </a
-          >.
-        </p>
-        <p>
-          If you have questions, please join the official
+            Nuxt Documentation
+          </a>
+          <br />
           <a
-            href="https://chat.vuetifyjs.com/"
+            href="https://github.com/nuxt/nuxt.js"
             target="_blank"
             rel="noopener noreferrer"
-            title="chat"
           >
-            discord </a
-          >.
-        </p>
-        <p>
-          Find a bug? Report it on the github
-          <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="contribute"
-          >
-            issue board </a
-          >.
-        </p>
-        <p>
-          Thank you for developing with Vuetify and I look forward to bringing
-          more exciting features in the future.
-        </p>
-        <div class="text-xs-right">
-          <em><small>&mdash; John Leider</small></em>
-        </div>
-        <hr class="my-3" />
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nuxt Documentation
-        </a>
-        <br />
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nuxt GitHub
-        </a>
-      </v-card-text>
-      <!-- v-btnやv-menuなどのカードのactionsを配置するためのコンテナ -->
-      <v-card-actions>
-        <v-spacer />
-        <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-col>
-</v-row>
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <!-- v-btnやv-menuなどのカードのactionsを配置するためのコンテナ -->
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
