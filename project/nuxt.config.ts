cd9909146aa2f94238a25aee81cd12463b01085e
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-01-24 13:36:22 +0900

    [fix] タブの修正

--- project/nuxt.config.ts
+++ project/nuxt.config.ts
@@ -7,8 +7,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - project',
-    title: 'project',
+    // titleTemplate: '%s - project',
+    title: 'Slack Trend',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
