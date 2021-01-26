c3d33787ed44cc40bbd49eaf7391e2e29b623540
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-01-26 20:33:22 +0900

    [add] v-card-textの中央寄せ

--- project/src/pages/index.vue
+++ project/src/pages/index.vue
@@ -29,7 +29,7 @@
                 </template>
 
                 <v-card>
-                  <v-card-title>チャンネルを選択</v-card-title>
+                  <v-card-title class="justify-center">チャンネルを選択</v-card-title>
                   <v-divider></v-divider>
                   <v-card-text style="height: 300px;">
                     <v-checkbox
@@ -41,19 +41,13 @@
                   </v-card-text>
                   <v-divider></v-divider>
                   <v-card-actions>
+                    <v-spacer></v-spacer>
                     <v-btn
                       color="blue darken-1"
                       text
                       @click="dialog = false"
                     >
-                      Close
-                    </v-btn>
-                    <v-btn
-                      color="blue darken-1"
-                      text
-                      @click="dialog = false"
-                    >
-                      Save
+                      閉じる
                     </v-btn>
                   </v-card-actions>
                 </v-card>
@@ -212,6 +206,14 @@ export default {
       {
         active: true,
         name: 'agenda'
+      },
+      {
+        active: false,
+        name: 'times_hayashi'
+      },
+      {
+        active: false,
+        name: 'times_test'
       },
     ],
     files: [
